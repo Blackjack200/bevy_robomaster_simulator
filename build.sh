91802d213500efc0882f263eac06ffcd1cac38ea
Blackjack200 <lx826444126@gmail.com>
2025-11-23 15:21:15 +0800

    more change

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-env DYLD_LIBRARY_PATH="/Users/blackjack/micromamba/envs/ros_env/lib:$DYLD_LIBRARY_PATH" \
-    zsh -i -c "micromamba activate ros_env && cargo run --release"
+export DYLD_LIBRARY_PATH=/Users/blackjack/micromamba/envs/ros_env/lib:/Users/blackjack/micromamba/envs/ros_env/opt/rviz_ogre_vendor/lib;
+export ROS_DISTRO=humble;
+export AMENT_PREFIX_PATH=/Users/blackjack/micromamba/envs/ros_env;
+cargo run --release --verbose
