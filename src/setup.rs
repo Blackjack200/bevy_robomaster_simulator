25bfd79b2605aeb173b5a5dfb2df556138ccee32
Blackjack200 <lx826444126@gmail.com>
2026-05-06 19:22:57 +0800

    Remove explicit clear color from capture and preview cameras

--- src/setup.rs
+++ src/setup.rs
@@ -155,7 +155,7 @@ pub fn setup(
             is_active: false,
             #[cfg(not(any(feature = "ros2", feature = "talos")))]
             is_active: config.preview.enabled,
-            clear_color: ClearColorConfig::Custom(Color::BLACK),
+            // clear_color: ClearColorConfig::Custom(Color::BLACK),
             ..default()
         },
         Projection::Perspective(PerspectiveProjection {
